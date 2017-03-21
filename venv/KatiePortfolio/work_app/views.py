b57633b494f3fdfa25797ece0b154d2e0d9c3bda
Kirk-SEATX <Kirk-SEATX@users.noreply.github.com>
2017-03-21 15:24:35 -0500

    Commit working app

--- venv/KatiePortfolio/work_app/views.py
+++ venv/KatiePortfolio/work_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from work_app.models import Project
 
 # Create your views here.
+
+
+def index(request):
+    works = Project.objects.all()
+    return render(request, 'index.html', {'works':works})
+
